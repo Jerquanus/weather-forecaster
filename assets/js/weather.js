b3fe0f08b711c71dd4a15df4553c8b7cdd58d334
Jerquanus Swann <jerquanus@gmail.com>
2021-02-01 13:05:34 -0500

    api key added

---
+++ assets/js/weather.js
@@ -0,0 +1,6 @@
+var apiKey ="c503307ff7b15c786a04498cfc96f843";
+
+function buildQueryURL() {
+    // queryURL is the url we'll use to query the API
+    var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?";
+}
